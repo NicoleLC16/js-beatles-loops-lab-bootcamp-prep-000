ca76abec2a076b86a0e559f7ab32dddc22494f7f
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-05 16:06:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["guitar", "bass", "guitar", "drums"];
 
 function theBeatlesPlay (musicians, instruments) {
   var emptyArray = [];
