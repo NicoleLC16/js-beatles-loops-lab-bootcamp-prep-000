8f8abd97b5151c5742d1ff7373fee7d452b097bd
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-05 16:13:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["guitar", "bass", "guitar", "drums"];
-
-function theBeatlesPlay (musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (let i = 0; i < musician.length; i++);
-  
-  array.push(`${musicians[i]} plays ${instruments[i]}`);
-  
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
   return array;
 }
