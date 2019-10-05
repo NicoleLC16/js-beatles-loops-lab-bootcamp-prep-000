0420444ab7fb58a69e50adc55c6607fab70afeb6
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-05 15:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
 function theBeatlesPlay (musicians, instruments) {
   var emptyArray = ["John Lennon plays guitar"];
   for (let i=0; i < 3; i++) {
-    console.log (`$(musicians) plays $(instruments)`);
     
     if (i === 0) {
       console.log (emptyArray);
       
       else {
-        
+        console.log (`$(musicians) plays $(instruments)`);
       }
     }
   }
