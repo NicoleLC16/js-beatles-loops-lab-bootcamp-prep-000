ded26e4f51e8dd63fcee07743e05eca29526d240
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-05 15:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ function theBeatlesPlay (musicians, instruments) {
   var emptyArray = [];
   for (let i=0; i < musicians.length; i++) {
     
-    if (i === 0) {
-      console.log (emptyArray);
-    }
-      else {
-        console.log (`$(musicians) plays $(instruments)`);
+  `$(musicians) plays $(instruments)`
       
     }
-  }
   
 }
